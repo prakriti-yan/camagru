fdbee48cd0edcc95bcc1670113f453d44cc0da09
Yan Yan <yyan@c2r2p4.hive.fi>
2020-09-21 15:23:15 +0300

    initial

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php
+$DB_DSN = 'mysql:host=localhost';
+$DB_USER = 'root';
+$DB_PD = 'password';
+$DB_NAME = 'camagru';
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?PHP session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Camagru</title>
+</head>
+<body>
+
+<p>Camgagu</p>
+	
+</body>
+</html>
