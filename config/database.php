f946b399bd05f4c3b4e4e8714b08cbce82a36aa6
Yan Yan <yyan@c2r2p4.hive.fi>
2020-09-25 15:30:59 +0300

    setup tables

--- config/database.php
+++ config/database.php
@@ -1,8 +1,7 @@
 <?php
 
 $DB_DSN = 'mysql:dbname=camagru;host=127.0.0.1';
-// $DB_DSN = 'mysql:dbname=camagru;port=8889;host=localhost';
 $DB_USER = 'root';
-$DB_PD = 'password';
+$DB_PASSWORD = 'password';
 
 ?>
--- config/setup.php
+++ config/setup.php
@@ -2,27 +2,57 @@
 
 require 'database.php';
 
-// $DB_DSN = 'mysql:dbname=camagru;host=127.0.0.1';
-// $DB_USER = 'root';
-// $DB_PD = 'password';
-
 try{
-	$conn = new PDO($DB_DSN, $DB_USER, $DB_PD);
+	$conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// $sql = "USE camagru";
-	// $conn->exec($sql);
-	// $sql = 'CREATE TABLE IF NOT EXISTS product (
-	// 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	// 	name VARCHAR(50) NOT NULL,
-	// 	information TEXT NOT NULL,
-	// 	price INT NOT NULL,
-	// 	category VARCHAR(100) NOT NULL,
-	// 	origin VARCHAR(100) NOT NULL,
-	// 	img VARCHAR(300) NOT NULL);';
-	// $conn->query($sql);
+	$sql = 'CREATE DATABASE IF NOT EXISTS camagru';
+	$conn->exec($sql);
 }catch(PDOException $e){
 	die('Error: '.$e->getMessage());
 }
 
+try{
+	$conn = new PDO($DB_DSN, $DB_USER, $DB_PD);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$sql = 'CREATE TABLE IF NOT EXISTS `users` (
+		`id_user` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		`login` VARCHAR(30) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+		`password` VARCHAR(255) NOT NULL,
+		`email` VARCHAR(255) NOT NULL,
+		`date_creation` DATETIME NOT NULL,
+		`confirm` BOOLEAN DEFAULT 0 NOT NULL,
+		`token` VARCHAR(255),
+		`token_expires` DATETIME)';
+	$conn->exec($sql);
+
+	$sql = "CREATE TABLE IF NOT EXISTS `images` (
+		`id_pic` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		`login` VARCHAR(30) NOT NULL,
+		`date_creation` DATETIME NOT NULL,
+		`image` LONGBLOB NOT NULL)";
+	$conn->exec($sql);
+
+	$sql = "CREATE TABLE IF NOT EXISTS `likes` (
+		`id_like` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		`id_pic` INT UNSIGNED NOT NULL,
+		`login` VARCHAR(30) NOT NULL,
+		`date_creation` DATETIME NOT NULL)";
+	$conn->exec($sql);
+
+	$sql = "CREATE TABLE IF NOT EXISTS `comments` (
+		`id_comment` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		`id_pic` INT UNSIGNED NOT NULL,
+		`comment` VARCHAR(255) NOT NULL,
+		`login` VARCHAR(30) NOT NULL,
+		`date_creation` DATETIME NOT NULL)";
+	$conn->exec($sql);
+
+
+}catch(PDOException $e){
+	die('Error: '.$e->getMessage());
+}
+
+$conn = null;
 
 ?>
